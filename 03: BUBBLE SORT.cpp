2554bf24a676aa10fe012c9134c62c9f0675d370
nooneknows199 <69891196+nooneknows199@users.noreply.github.com>
2021-05-05 18:45:22 +0530

    Update 03: BUBBLE SORT.cpp

--- 03: BUBBLE SORT.cpp
+++ 03: BUBBLE SORT.cpp
@@ -1,24 +1,36 @@
-#include<iostream>         // BUBBLE SORT
+#include<bits/stdc++.h>         // BUBBLE SORT
 using namespace std;       // T.C : O(n) / O(n^2)
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   bool swapped;
+   for (i = 0; i < n-1; i++)
+   {
+     swapped = false;
+     for (j = 0; j < n-i-1; j++)
+     {
+        if (arr[j] > arr[j+1])
+        {
+           swap(arr[j], arr[j+1]);
+           swapped = true;
+        }
+     }
+  
+     // IF no two elements were swapped by inner loop, then break
+     if (swapped == false)
+        break;
+   }
+}
+
 int main(){
-int n,i,arr[50],j,temp;
-cout<<"enter the array size: ";
+
+int n,arr[50],j;
 cin>>n;
-cout<<"enter array elements:\n";
-for(i=0;i<n;i++){
+for(int i=0;i<n;i++){
     cin>>arr[i];
 }
-for(i=0;i<(n-1);i++)
-{
-for(j=0;j<(n-i-1);j++)
-{
-    if(arr[j]>arr[j+1])
-    swap(arr[j], arr[j+1]);
-
-}
-}
+bubbleSort(arr, n);
 cout<<"sorted list is :\n";
-for(i=0;i<n;i++)
+for(int i=0;i<n;i++)
 cout<<arr[i]<<" ";
-
 }
