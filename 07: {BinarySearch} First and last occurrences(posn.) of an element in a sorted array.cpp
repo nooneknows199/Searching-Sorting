989a7e116ad26fae2926ebee02665226ca412d6e
KanchanVerma <69891196+nooneknows199@users.noreply.github.com>
2021-11-11 20:00:28 +0530

    Update 07: {BinarySearch} First and last occurrences(posn.) of an element in a sorted array.cpp

--- 07: {BinarySearch} First and last occurrences(posn.) of an element in a sorted array.cpp
+++ 07: {BinarySearch} First and last occurrences(posn.) of an element in a sorted array.cpp
@@ -20,43 +20,47 @@ while(t--){
 OR
 
 // By using BINARY SEARCH :
-#include <bits/stdc++.h>
-using namespace std;
 
-int solve1(int a[],int n,int x,int tog){
-    int first=0;
-    int last=n-1;
-    int ans = -1;
-    while(first<=last){
-    int  middle = first+(last-first)/2;
+class Solution {
+public:
+   vector<int> searchRange(vector<int>& nums, int target) {
+       vector<int> ans={-1,-1};
+        int n=nums.size();
+        if(n==0) return ans;   
+        ans[0]=firstPos(nums,0,n-1,target);
+        ans[1]=lastPos(nums,0,n-1,target);
+        return ans;
+    }
     
-        if (a[middle]>x)
-         last = middle - 1;
-		
-		else if (a[middle]<x)
-            first=middle+1;
-		else
-		{
-			 ans = middle;
-			 if(tog ==-1) last = middle + tog;
-			 else  first = middle + tog ;
-		}
-	}
-	return ans;
-}
-
-int main() {
-int t;
-cin>>t;
-while(t--){
-    int n,x;
-    cin>>n>>x;
-    int a[n];
-    for(int i=0;i<n;i++)  cin>>a[i];
-    int idx1 = solve1(a,n,x,-1);
-    int idx2 = solve1(a,n,x,1);
-    if(idx1 == n) cout<<"-1\n";
-    else cout<<idx1<<" "<<idx2<<endl;
-}
-
-}
+    int firstPos(vector<int>&nums,int lo,int hi,int target){
+        while(lo<=hi){
+           int mid=lo+(hi-lo)/2; 
+            if(nums[mid]<target){
+                lo=mid+1;
+            }
+            else if(nums[mid]>target) hi=mid-1;
+            
+            else{  //If mid is target element
+                if(mid==0 || nums[mid-1]<nums[mid]) return mid;  //if mid is first occurrence of target then either it is the first element of array i.e. it has no element before it, or element before mid element is smaller than it
+                else hi=mid-1; //if mid is not first occurrence then find in left half
+            }
+        }
+        return -1;
+    }
+    
+    int lastPos(vector<int>&nums,int lo,int hi,int target){
+         while(lo<=hi){
+           int mid=lo+(hi-lo)/2; 
+            if(nums[mid]<target){
+                lo=mid+1;
+            }
+            else if(nums[mid]>target) hi=mid-1;
+            else{  //If mid is target element
+                if(mid==nums.size()-1 || nums[mid+1]>nums[mid]) return mid; //if mid is last occurrence of target then either it is the last element of array i.e. it has no element after it, or element after mid element is greater than it
+                else lo=mid+1; //if element is not the last occurrence,then find in right half
+            }
+        }
+        return -1;
+    }
+    
+};
